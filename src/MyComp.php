e8cf9c85c2de742e876837f72642fee376f24264
Anton Luzhbin <antonluzhbin@yandex.ru>
2018-10-12 12:38:47 +0400

    first commit

---
+++ src/MyComp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 12.10.18
+ * Time: 12:32
+ */
+
+namespace MyComp;
+
+
+class MyComp
+{
+    public static function test()
+    {
+        echo 'MyComp testing';
+    }
+}
